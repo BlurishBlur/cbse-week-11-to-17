6ee2dcbb0fb978a07cdb567005ba24c9169e7016
Niels Heltner <niels.heltner@hotmail.com>
2017-04-25 09:37:01 +0200

    silentupdate slået fra, prøvet at fixe osgi dependency injection

--- AsteroidsNetBeansUpdate/OSGiAsteroid/src/main/java/dk/sdu/mmmi/cbse/osgiasteroid/AsteroidControlSystem.java
+++ AsteroidsNetBeansUpdate/OSGiAsteroid/src/main/java/dk/sdu/mmmi/cbse/osgiasteroid/AsteroidControlSystem.java
@@ -15,9 +15,8 @@ import java.util.List;
  *
  * @author Niels
  */
-public class AsteroidControlSystem implements IEntityProcessingService, IGamePluginService {
+public class AsteroidControlSystem implements IEntityProcessingService {
 
-    private List<Entity> asteroids = new ArrayList<>();
     private IAsteroidSplitter asteroidSplitter;
 
     @Override
@@ -38,7 +37,7 @@ public class AsteroidControlSystem implements IEntityProcessingService, IGamePlu
                     for (int i = 0; i < 2; i++) {
                         Entity newAsteroid = asteroidSplitter.createSplitAsteroid(asteroid);
                         world.addEntity(newAsteroid);
-                        asteroids.add(newAsteroid);
+                        //asteroids.add(newAsteroid);
                     }
                 }
                 world.removeEntity(asteroid);
@@ -76,38 +75,5 @@ public class AsteroidControlSystem implements IEntityProcessingService, IGamePlu
         System.out.println("AsteroidSplitter remove");
     }
 
-    @Override
-    public void start(GameData gameData, World world) {
-        for (int i = 0; i < (int) (Math.random() * 5) + 1; i++) { // creates a random amount of asteroids between 1 and 6, at the start
-            Entity asteroid = createWholeAsteroid(gameData);
-            world.addEntity(asteroid);
-            asteroids.add(asteroid);
-        }
-    }
-
-    private Entity createWholeAsteroid(GameData gameData) {
-        Entity asteroid = new Asteroid();
-        asteroid.setPosition((float) Math.random() * gameData.getDisplayWidth(), (float) Math.random() * gameData.getDisplayHeight());
-        asteroid.setRadius(((int) (Math.random() * 10) + 10));
-        asteroid.setLife(2);
-        asteroid.setMaxSpeed(((float) Math.random() * 120) + 10);
-        asteroid.setRadians((float) Math.random() * 3.1415f * 2);
-        asteroid.setRotationSpeed((int) (Math.random() * 3) - 1); //-1, 0 eller 1
-        asteroid.setDx((float) Math.cos(asteroid.getRadians()) * asteroid.getMaxSpeed());
-        asteroid.setDy((float) Math.sin(asteroid.getRadians()) * asteroid.getMaxSpeed());
-        int size = (int) (asteroid.getRadius() / 2) + 5;
-        float[] dists = new float[size];
-        for (int i = 0; i < size; i++) {
-            dists[i] = (float) (Math.random() * (asteroid.getRadius() / 2)) + (asteroid.getRadius() / 2);
-        }
-        asteroid.setDists(dists);
-        return asteroid;
-    }
-
-    @Override
-    public void stop(GameData gameData, World world) {
-        for (Entity asteroid : asteroids) {
-            world.removeEntity(asteroid);
-        }
-    }
+    
 }

e081e186cb26d1588f80fd2e0466af030fc86866
Niels Heltner <niels.heltner@hotmail.com>
2017-04-28 21:40:53 +0200

    integration test implementeret

--- AsteroidsNetBeansTest/application/src/test/java/dk/sdu/mmmi/cbse/ApplicationTest.java
+++ AsteroidsNetBeansTest/application/src/test/java/dk/sdu/mmmi/cbse/ApplicationTest.java
@@ -1,11 +1,25 @@
 package dk.sdu.mmmi.cbse;
 
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import java.io.IOException;
+import static java.nio.file.Files.copy;
+import static java.nio.file.Paths.get;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import junit.framework.Test;
 import org.netbeans.junit.NbModuleSuite;
 import org.netbeans.junit.NbTestCase;
+import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
 
 public class ApplicationTest extends NbTestCase {
+    
+    private static final String UPDATES_FILE = "D:\\Dropbox\\Java projekter\\cbse-week-11-to-17\\AsteroidsNetBeansTest\\netbeans_site\\updates.xml";
+    private static final String UPDATES_FILE_ADD_ENEMY = "D:\\Dropbox\\Java projekter\\cbse-week-11-to-17\\AsteroidsNetBeansTest\\application\\src\\test\\resources\\addenemy\\updates.xml";
+    private static final String UPDATES_FILE_REMOVE_ENEMY = "D:\\Dropbox\\Java projekter\\cbse-week-11-to-17\\AsteroidsNetBeansTest\\application\\src\\test\\resources\\removeenemy\\updates.xml";
 
     public static Test suite() {
         return NbModuleSuite.createConfiguration(ApplicationTest.class).
@@ -21,12 +35,40 @@ public class ApplicationTest extends NbTestCase {
         super(n);
     }
 
-    public void testApplication() {
-        // pass if there are merely no warnings/exceptions
-        /* Example of using Jelly Tools (additional test dependencies required) with gui(true):
-        new ActionNoBlock("Help|About", null).performMenu();
-        new NbDialogOperator("About").closeByButton();
-         */
+    public void testApplication() throws IOException {
+        List<IEntityProcessingService> processors = new CopyOnWriteArrayList<>();
+        List<IGamePluginService> plugins = new CopyOnWriteArrayList<>();
+        copy(get(UPDATES_FILE_REMOVE_ENEMY), get(UPDATES_FILE), REPLACE_EXISTING);
+        waitForUpdate(processors, plugins);
+        
+        assertEquals("No processors", 0, processors.size());
+        assertEquals("No plugins", 0, plugins.size());
+        
+        copy(get(UPDATES_FILE_ADD_ENEMY), get(UPDATES_FILE), REPLACE_EXISTING);
+        waitForUpdate(processors, plugins);
+        
+        assertEquals("One processor", 1, processors.size());
+        assertEquals("One plugin", 1, plugins.size());
+        
+        copy(get(UPDATES_FILE_REMOVE_ENEMY), get(UPDATES_FILE), REPLACE_EXISTING);
+        waitForUpdate(processors, plugins);
+        
+        assertEquals("No processors", 0, processors.size());
+        assertEquals("No plugins", 0, plugins.size());
+    }
+
+    private void waitForUpdate(List<IEntityProcessingService> processors, List<IGamePluginService> plugins) {
+        try {
+            Thread.sleep(10000);
+            processors.clear();
+            processors.addAll(Lookup.getDefault().lookupAll(IEntityProcessingService.class));
+            
+            plugins.clear();
+            plugins.addAll(Lookup.getDefault().lookupAll(IGamePluginService.class));
+        }
+        catch (InterruptedException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 
 }
